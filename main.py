2c066439811899e973f3eaf60bb41a4ebdd32da2
nejvad <jenesesdavid620@gmail.com>
2024-06-29 20:50:09 +0200

    script sorts by extension and date

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
-from os.path import exists
+from time import ctime
+from os.path import exists, getctime
 
 
 def main():
@@ -18,15 +19,25 @@ def create_folders(download, sorted_download):
 
     for file in files:
         current_path = sorted_download + "\\" + file.split(".")[-1]
-        if not exists(current_path):
-            os.mkdir(current_path)
+        # TODO look for a more elegant solution
+        file_creation_date = "_".join(str(ctime(getctime(download + "\\" + file))).split(" ")[:3])
+        current_path_with_creation_date = current_path + "-" + file_creation_date
+
+        if not is_folder_already_exists(current_path_with_creation_date):
+            os.mkdir(current_path_with_creation_date)
 
 
 def sort_files(download, sorted_download):
     files = os.listdir(download)
 
     for file in files:
-        shutil.move(download + "\\" + file, sorted_download + "\\" + file.split(".")[1])
+
+        file_creation_date = "_".join(str(ctime(getctime(download + "\\" + file))).split(" ")[:3])
+        shutil.move(download + "\\" + file, sorted_download + "\\" + file.split(".")[1] + "-" + file_creation_date)
+
+
+def is_folder_already_exists(path):
+    return exists(path)
 
 
 if __name__ == "__main__":
