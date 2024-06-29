336060c41c27f79b470817778cf6c54bccec1ef6
nejvad <jenesesdavid620@gmail.com>
2024-06-29 18:12:26 +0200

    gather file extensions in a set

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from os import listdir
+
+
 def main():
-    return ""
+    download_folder_path = "C:\\Users\\jenes\\Downloads"
+    files = listdir(download_folder_path)
+    extensions = set()
+
+    for file in files:
+        extensions.add(file.split(".")[-1])
+
+    print(extensions)
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     main()
