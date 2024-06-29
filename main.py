06fd5486fa95a3706ee8925f20b57b18a3a9eb43
nejvad <jenesesdavid620@gmail.com>
2024-06-29 18:30:49 +0200

    create empty destination folder

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from os import listdir
+import os
+from os.path import exists
 
 
 def main():
     download_folder_path = "C:\\Users\\jenes\\Downloads"
-    files = listdir(download_folder_path)
+    sorted_files_path = "C:\\Users\\jenes\\Sorted Downloads"
+    files = os.listdir(download_folder_path)
     extensions = set()
+
+    if not exists(sorted_files_path):
+        os.mkdir(sorted_files_path)
 
     for file in files:
         extensions.add(file.split(".")[-1])
-
-    print(extensions)
 
 
 if __name__ == "__main__":
