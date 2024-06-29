8f9edbcc32575c49629736a2709196a7c205fdba
nejvad <jenesesdavid620@gmail.com>
2024-06-29 20:02:00 +0200

    created functions

--- main.py
+++ main.py
@@ -6,20 +6,27 @@ from os.path import exists
 def main():
     download_folder_path = "C:\\Users\\jenes\\Downloads"
     sorted_files_path = "C:\\Users\\jenes\\Sorted Downloads"
-    files = os.listdir(download_folder_path)
-    extensions = set()
+    create_folders(download_folder_path, sorted_files_path)
+    sort_files(download_folder_path, sorted_files_path)
 
-    if not exists(sorted_files_path):
-        os.mkdir(sorted_files_path)
 
-    for file in files:
-        extensions.add(file.split(".")[-1])
+def create_folders(download, sorted_download):
+    files = os.listdir(download)
 
-    for extension in extensions:
-        os.mkdir(sorted_files_path + "\\" + extension)
+    if not exists(sorted_download):
+        os.mkdir(sorted_download)
 
     for file in files:
-        shutil.move(download_folder_path + "\\" + file, sorted_files_path + "\\" + file.split(".")[-1])
+        current_path = sorted_download + "\\" + file.split(".")[-1]
+        if not exists(current_path):
+            os.mkdir(current_path)
+
+
+def sort_files(download, sorted_download):
+    files = os.listdir(download)
+
+    for file in files:
+        shutil.move(download + "\\" + file, sorted_download + "\\" + file.split(".")[1])
 
 
 if __name__ == "__main__":
